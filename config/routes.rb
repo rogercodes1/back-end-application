563afa38596710146b6920d5aa7c06aa3910b404
rogercodes1 <roger.ucsb@gmail.com>
2018-06-25 10:12:09 -0400

    updated namespace in route files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json }, path: '/api'  do
+  # namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create]
       resources :bills, only: [:index, :create, :show, :destroy]
